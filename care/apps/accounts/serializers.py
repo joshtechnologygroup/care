342c736116d94a85edcd77f73665f1f15a28f80c
Ayush Sidana <ayush.sidana@joshtechnolgygroup.com>
2020-05-22 22:30:43 +0530

    remove unused serializer

--- care/apps/accounts/serializers.py
+++ care/apps/accounts/serializers.py
@@ -6,37 +6,18 @@ from rest_framework.authtoken.models import Token
 from apps.accounts import models as accounts_models
 from django.contrib.auth import password_validation, hashers
 
-
-class UserInfoUpdateSerializer(rest_serializers.ModelSerializer):
-
-    class Meta:
-        model = accounts_models.User
-        fields = ('first_name', 'last_name', 'phone_number',)
-
-
-class PasswordUpdateSerializer(rest_serializers.ModelSerializer):
-
-    class Meta:
-        model = accounts_models.User
-        fields = ('password',)
-        extra_kwargs = {
-            'password': {'write_only': True, 'required': True},
-        }
+class UserSerializer(rest_serializers.ModelSerializer):
 
     def validate_password(self, password):
         password_validation.validate_password(password)
         password = hashers.make_password(password)
         return password
 
-
-class UserSerializer(PasswordUpdateSerializer, UserInfoUpdateSerializer):
-
     class Meta:
         model = accounts_models.User
-        fields = ('district', 'user_type', 'email',) + \
-            PasswordUpdateSerializer.Meta.fields + UserInfoUpdateSerializer.Meta.fields
+        fields = ('district', 'user_type', 'email','first_name', 'last_name', 'phone_number','password',)
         extra_kwargs = {
-            'password': {'write_only': False, 'required': False},
+            'password': {'write_only':True, 'required': False},
             'user_type': {'write_only': True}
         }
 

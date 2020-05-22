7cc841d4da37a9fad30a4eb26786dc1eb8d7d497
Ayush Sidana <ayush.sidana@joshtechnolgygroup.com>
2020-05-22 18:31:15 +0530

    added serializers for get user details list and password

--- care/apps/accounts/serializers.py
+++ care/apps/accounts/serializers.py
@@ -1,10 +1,49 @@
 from rest_framework.serializers import ModelSerializer
 
 from apps.accounts import models as accounts_models
+from django.contrib.auth import password_validation, hashers
 
-
-class UserSerializer(ModelSerializer):
+class UserInfoUpdateSerializer(ModelSerializer):
 
     class Meta:
         model = accounts_models.User
-        fields = '__all__'
+        fields = ('first_name','last_name', 'phone_number')
+        # extra_kwargs = {
+        #     'district': {'write_only': True},
+        # }
+    def validate(self, attrs):
+        print('aaaaa')
+        # attrs['manager'] = self.context['request'].user
+        return attrs    
+
+class PasswordUpdateSerializer(ModelSerializer):
+
+    class Meta:
+        model = accounts_models.User
+        fields = ('password',)
+
+    def validate(self, attrs):
+        print(attrs,"password")
+        password_validation.validate_password(data['password'])
+        data['password'] = hashers.make_password(data['password'])
+        return attrs          
+
+
+class UserSerializer(PasswordUpdateSerializer, UserInfoUpdateSerializer):
+
+    class Meta:
+        model = accounts_models.User
+        fields = ('district','user_type',) + PasswordUpdateSerializer.Meta.fields + UserInfoUpdateSerializer.Meta.fields
+        extra_kwargs = {
+            'district': {'write_only': True},
+            'password': {'write_only': True},
+        }
+
+    # def validate(self, attrs):
+    #     return super().validate(attrs)
+    #     attrs.validate_password
+    #     attrs['manager'] = self.context['request'].user
+    #     return attrs        
+
+    def update(self, instance, validated_data):
+        return super(UserSerializer, self).update(instance, validated_data)
--- care/apps/accounts/views.py
+++ care/apps/accounts/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import permissions
 from apps.accounts import (
     models as accounts_models,
     serializers as accounts_serializers
@@ -10,4 +10,8 @@ class UserViewSet(ModelViewSet):
 
     queryset = accounts_models.User.objects.all()
     serializer_class = accounts_serializers.UserSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+    # def get_queryset(self):
+    #     return accounts_models.objects.filter(user=self.request.user)
 

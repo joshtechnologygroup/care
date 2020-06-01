7413470c348d4219fef043fe79a296906b04ae4c
SHIVAM JINDAL <shivam.jindal@joshtechnologygroup.com>
2020-06-01 20:47:10 +0530

    override get_object

--- care/apps/patients/views.py
+++ care/apps/patients/views.py
@@ -46,6 +46,34 @@ class PatientViewSet(rest_viewsets.ModelViewSet):
         self.serializer_class = patient_serializers.PatientDetailsSerializer
         return super().retrieve(request, *args, **kwargs)
 
+    def get_object(self):
+        """
+        Returns the object the view is displaying.
+
+        You may want to override this if you need to provide non-standard
+        queryset lookups.  Eg if objects are referenced using multiple
+        keyword arguments in the url conf.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = queryset.filter(**filter_kwargs).first()
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
     def get_queryset(self):
         queryset = patient_models.Patient.objects.all()
         if self.request.user.user_type and self.request.user.user_type.name == commons_constants.PORTEA:
@@ -64,8 +92,6 @@ class PatientViewSet(rest_viewsets.ModelViewSet):
             ownership_type=F("patientfacility__facility__owned_by__name"),
             facility_district=F("patientfacility__facility__district__name"),
         )
-        if self.action == 'retrieve':
-            queryset = queryset[:1]
         return queryset
 
 
